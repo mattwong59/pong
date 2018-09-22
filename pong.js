aa62dd5e77c2ffa8f8aeee69c8baaf5b7dd37caf
Matt Wong <mattwong59@gmail.com>
2018-09-22 13:14:38 -0700

    Setup game area

---
+++ pong.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById('pong');
+const context = canvas.getContext('2d');
+
+context.fillStyle = '#000';
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+context.fillStyle = '#FFF';
+context.fillRect(0, 0, 20, 20);
+
