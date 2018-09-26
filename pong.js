a4f54a8eec2abdeb14cfd26101ac0316887783d7
Matt Wong <mattwong59@gmail.com>
2018-09-25 18:11:42 -0700

    Draw score on game board

--- pong.js
+++ pong.js
@@ -74,6 +74,37 @@ class Pong {
             requestAnimationFrame(callback);    //animation function
         };
         callback();
+
+        this.CHAR_PIXEL = 10;
+        this.CHARS = [
+            '111101101101111', 
+            '010010010010010',
+            '111001111100111',
+            '111001111001111',
+            '101101111001001',
+            '111100111001111',
+            '111100111101111',
+            '111001001001001',
+            '111101111101111',
+            '111101111001111'
+        ].map(str => {
+            const canvas = document.createElement('canvas');
+            canvas.height = this.CHAR_PIXEL * 5;
+            canvas.width = this.CHAR_PIXEL * 3;
+            const context = canvas.getContext('2d');
+            context.fillStyle = '#fff';
+            str.split('').forEach((fill, i) => {
+                if (fill === '1') {
+                    context.fillRect(
+                        (i % 3) * this.CHAR_PIXEL, 
+                        (i / 3 | 0) * this.CHAR_PIXEL,      //gives us top left corner to paint pixel
+                        this.CHAR_PIXEL,
+                        this.CHAR_PIXEL);
+                }
+            });
+            return canvas;
+        });
+
         this.reset();
     }
 
@@ -95,12 +126,30 @@ class Pong {
         this.drawRect(this.ball);
         
         this.players.forEach(player => this.drawRect(player));
+
+        this.drawScore();
     }
 
     drawRect(rect) {
         this._context.fillStyle = '#FFF';
         this._context.fillRect(rect.left, rect.top, 
                                rect.size.x, rect.size.y);     //draws the ball
+    }
+
+    drawScore() {
+        const align = this._canvas.width / 3;
+        const CHAR_W = this.CHAR_PIXEL * 4;
+        this.players.forEach((player, index) => {
+            const chars = player.score.toString().split('');
+            const offset = align * 
+                           (index + 1) - 
+                           (CHAR_W * chars.length / 2) * 
+                           this.CHAR_PIXEL / 2;
+            chars.forEach((char, pos) => {
+                this._context.drawImage(this.CHARS[char | 0],
+                                        offset + pos * CHAR_W, 20);
+            });
+        });
     }
 
     reset() {
